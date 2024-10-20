81fbc6439de58a11c27b4684495bfbfabeda5cdd
Daniel Mauderer <daniel.mauderer@outlook.com>
2024-10-20 12:14:52 +0200

    start filling with content

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,7 +7,9 @@ interface Project {
   title?: string;
   description?: string;
   image?: string;
+  languages?: string[];
   technologies?: string[];
+  other?: string[];
 }
 
 @Component({
@@ -22,9 +24,12 @@ export class ProjectsComponent {
 
   constructor() {
     this.projects = [
-      { title: 'Project 1', description: 'This is the first project.', image: 'DSCF1676.JPG', technologies: ['Angular', 'PrimeNG'] },
-      { title: 'Project 2', description: 'This is the second project.', image: 'DSCF1836.JPG', technologies: ['rust', 'angular', 'postgres'] },
-      { title: 'Project 3', description: 'This is the third project.', image: 'DSCF1994.JPG', technologies: ['C', 'OpenCl'] },
+      { title: 'GameServerManager', description: 'This is the first project.', image: 'gameserver_overview.jpg', languages: ['C#', 'TypeScript', 'CSS', 'HTML'], technologies: ['Vue', 'ASP.Net'], other: ['Docker', 'gRPC'] },
+      { title: 'OpenGreenhouseManager', description: 'This is the second project.', image: 'greenhouse_overview.jpg', languages: ['rust', 'TypeScript', 'CSS', 'HTML'], technologies: ['Angular', 'PrimeNG', 'axum', 'diesel'], other: ['Docker', 'PostgreSQL', 'GitHub Pipelines', 'ESP-32', 'raspberryPi', 'embedded'] },
+      { title: 'WildUhr', description: 'This is the third project.', image: 'WildUhr_overview.jpg', languages: ['C++', 'C#', 'TypeScript', 'CSS', 'HTML'], technologies: ['ASP.Net', 'esp-idf'], other: ['PCB design(KiCad)', 'ESP-32', 'CMake', 'embedded']  },
+      { title: 'portfolio', description: 'This is the third project.', image: 'portfolio_overview.jpg', languages: ['TypeScript'], technologies: ['Angular', 'PrimeNG'], other: []  },
+      { title: 'star', description: 'This is the third project.', image: 'stars_overview.jpeg', languages: ['rust', 'c'], technologies: ['OpenCL'], other: ['OpenCL - GPU', 'SIMD']  },
+      { title: 'fermentation-pi ', description: 'This is the third project.', image: 'Schimmelbox_overview.jpg', languages: ['rust', 'TypeScript', 'CSS', 'HTML'], technologies: ['rocket', 'Vue'], other: ['raspberryPi', 'embedded'] },
     ];
   }
 }
