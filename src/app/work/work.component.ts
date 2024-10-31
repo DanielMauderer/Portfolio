2a7a76c0e2c5bd4ee69878f7fec213a4ed831794
Daniel Mauderer <daniel.mauderer@outlook.com>
2024-10-31 21:37:08 +0100

    add work content

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -23,8 +23,16 @@ export class WorkComponent {
 
   constructor() {
         this.workPlaces = [
-          { startDate: '2022', endDate: 'today', icon: 'pi pi-briefcase', title: 'Perfood', description: 'Worked as a software engineer at a company.' },
-          { startDate: '2017', endDate: '2022', icon: 'pi pi-briefcase', title: 'AUNOVIS GmbH', description: 'Worked as a junior software engineer at a company.' },
+          {
+            startDate: '2022', endDate: 'today', icon: 'pi pi-briefcase', title: 'Perfood', description:
+`When I started at Perfood worked as a fullstack developer. I developed mobile apps and maintained backend services.
+
+At the moment I also develop an essential intern tool with the Django framework in python. My tasks include maintaining the CI/CD pipelines and managing releases.` },
+          {
+            startDate: '2017', endDate: '2022', icon: 'pi pi-briefcase', title: 'AUNOVIS GmbH', description:
+`During my studies I had a look in all departments. I gather experience in many different languages and technologies. The main focus was C#, C++, Typescript/JavaScript and HTML/CSS.
+
+After my studies I first worked as a C++ developer and later switched project to work as a full stack developer. Additionally I automated release processes and created a CI/CD pipeline in Jenkins.`},
           { startDate: '2017', endDate: '2020', icon: 'pi pi-graduation-cap', title: 'Duale Hochschule Baden-Württemberg (DHBW) Karlsruhe', description: 'Bachelor of Science - BS, Informatik' },
         ];
   }
